ad56a7a360077c4d0c2dc34f0cec44dafd21b478
Tynukua <bowsworddv@gmail.com>
2019-11-12 16:50:30 +0200

    snakeize

--- mono/types.py
+++ mono/types.py
@@ -1,6 +1,22 @@
+def snakeize_s(string):
+    answer = ''
+    for char in str(string):
+        if char.isupper():
+            answer += f'_{char.lower()}'
+        else:
+            answer += char
+    return answer
+
+def snakeize_dict(dict_):
+    answer = {}
+    for key in dict_:
+        key = snakeize_s(key)
+        answer[key] = dict_[key]
+    return answer
+    
 class MonoCard:
     def __init__(self, json):
-        self.__dict__ = json
+        self.__dict__ = snakeize_dict( json)
 
 class ClientInfo:
     def __init__(self, json):
