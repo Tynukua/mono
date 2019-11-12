537a13e49c3b929337edcbac55e84882645a6c91
Tynukua <bowsworddv@gmail.com>
2019-11-12 16:56:29 +0200

    __headers(), field HEADERS,

--- mono/__init__.py
+++ mono/__init__.py
@@ -1,22 +1,27 @@
 from requests import get
 from .types import *
-HEADERS = {
-    'Request Content-Types': 'application/json',
-    'Response Content-Types': 'application/json', 
-    'Schemes': 'https', 
-    'Version': '201906' }
     
 BASE_URL = 'https://api.monobank.ua'
 
 class OpenMono:
+    HEADERS = {
+        'Request Content-Types': 'application/json',
+        'Response Content-Types': 'application/json', 
+        'Schemes': 'https', 
+        'Version': '201906' }
+
     def __init__(self, token, version = None):
         self._TOKEN = token
         if version:
-            HEADERS['Version'] = str(version)
-    
+            OpenMono.HEADERS['Version'] = str(version)
+
+    @property
+    def __headers(self):
+        return {**OpenMono.HEADERS, **{'X-Token': self._TOKEN}}
+
     def client_info(self):
         response = get(BASE_URL+'/personal/client-info', 
-            headers = {**HEADERS, **{'X-Token': self._TOKEN} }).json() 
+            headers=self.__headers).json() 
         response['accounts'] = [ MonoCard(i) for i in response['accounts'] ] 
         return ClientInfo(response)
-    
+
