d9dbb888cbe76551e3d824730f5ebf1e6d6b32c7
Tynukua <bowsworddv@gmail.com>
2019-11-12 17:10:37 +0200

    as_json() for types

--- mono/types.py
+++ mono/types.py
@@ -17,11 +17,25 @@ def snakeize_dict(dict_):
 class MonoCard:
     def __init__(self, json):
         self.__dict__ = snakeize_dict( json)
+    
+    def as_dict(self):
+        return self.__dict__.copy()
 
 class ClientInfo:
     def __init__(self, json):
         self.__dict__ = snakeize_dict( json)
+    
+    def as_dict(self):
+        dict_ = self.__dict__.copy()
+        for key in dict_:
+            try:
+                dict_[key] = dict_[key].as_json()
+            except Exception:
+                pass
 
 class StatementItem:
     def __init__(self, json):
         self.__dict__ = snakeize_dict( json)
+
+    def as_dict(self):
+        return self.__dict__.copy()
