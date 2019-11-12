ca8495f4223efaa03156908d21d183f64ba62702
Tynukua <bowsworddv@gmail.com>
2019-11-12 16:24:57 +0200

    types of monobank's API

--- mono/types.py
+++ mono/types.py
@@ -1,1 +1,11 @@
-class ClientInfo()
+class MonoCard:
+    def __init__(self, json):
+        self.__dict__ = json
+
+class ClientInfo:
+    def __init__(self, json):
+        self.__dict__ = json
+
+class StatementItem:
+    def __init__(self, json):
+        self.__dict__ = json
